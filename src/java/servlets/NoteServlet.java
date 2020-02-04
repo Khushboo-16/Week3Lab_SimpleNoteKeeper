b3d415e73b9850d86d995d2a94a58b6baded34cb
Khushboo Patel <khushboo.patel@edu.sait.ca>
2020-02-03 21:07:41 -0700

    Initial commit

--- src/java/servlets/NoteServlet.java
+++ src/java/servlets/NoteServlet.java
@@ -11,6 +11,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.File;
+import domain.Note;
+import java.util.ArrayList;
 
 /**
  *
@@ -31,6 +38,56 @@ public class NoteServlet extends HttpServlet {
             throws ServletException, IOException {
         
     }
+    
+    private Note noteRead (HttpServletRequest request) throws IOException {
+        String title = "";
+        String contents = "";
+       
+        
+        ArrayList<String> notecontents = new ArrayList<>();
+        
+         //Get note.txt path
+         String path = getServletContext().getRealPath("/WEB-INF/note.txt");
+         // to read files
+        BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+        
+        while(br.ready()) {
+            notecontents.add(br.readLine());
+        }
+        
+        title = notecontents.get(0);
+        
+        for(int i=1; i < notecontents.size(); i++) {
+            contents += notecontents.get(i) + "\n";
+        }
+        
+        Note note = new Note(title, contents);
+        
+        br.close();
+        
+        return note;
+    }
+    
+    private Note noteWrite(HttpServletRequest request) throws IOException {
+        String formtitle = "";
+        String formcontent = "";
+        
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
+        
+        formtitle = request.getParameter("title");
+        formcontent = request.getParameter("contents");
+        // to write to a file
+       PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path, false))); 
+       
+       pw.printf("%s%n",formtitle);
+       pw.printf(formcontent);
+       
+       pw.close();
+       
+       Note n = new Note(formtitle, formcontent);
+       
+       return n;
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -46,19 +103,24 @@ public class NoteServlet extends HttpServlet {
             throws ServletException, IOException {
        // processRequest(request, response);
        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet NoteServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet NoteServlet at " + request.getContextPath() + "</h1>");
-            out.println("<h2>[GET]</h2>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+       
+       String edit = request.getParameter("edit");
+       
+       Note note = noteRead(request);
+       request.setAttribute("note", note);
+      // System.out.println("doGet():");
+       
+       if (edit != null) {
+           //System.out.println("Edit Mode");
+           getServletContext().getRequestDispatcher("/WEB-INF/editnote.jsp").forward(request, response);
+       }else {
+           //System.out.println("View Mode");
+           getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
+       }
+       
+       //response.setContentType("text/html;charset=UTF-8");
+       
+       
     }
 
     /**
@@ -74,19 +136,14 @@ public class NoteServlet extends HttpServlet {
             throws ServletException, IOException {
         //processRequest(request, response);
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet NoteServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet NoteServlet at " + request.getContextPath() + "</h1>");
-            out.println("<h2>[POST]</h2>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        
+        Note n = noteWrite(request);
+        request.setAttribute("note", n);
+        getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
+        //System.out.println("POST Request:");
+        //System.out.println("doPost():");
+       
+       
     }
 
     /**
